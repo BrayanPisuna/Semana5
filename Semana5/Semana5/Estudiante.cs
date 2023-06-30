d87eb009ed5724470580cde9f580ba2418919466
master <master@SMLAB108PC21_23>
2023-06-29 21:06:47 -0500

    Agregar archivos de proyecto.

---
+++ Semana5/Semana5/Estudiante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Semana5
+{
+    public class Estudiante
+    {
+        public int codigo { get; set;}
+        public int nombre { get; set; }
+
+        public int apellido { get; set; }
+
+        public int edad { get; set; }
+
+
+    }
+}
---
+++ Semana5/Semana5/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana5
+{
+    public partial class MainPage : ContentPage
+    {
+        private String url = "http://l192.168.10.47/ws_uisrael/post.php";
+        private readonly HttpClient cliente = new HttpClient();
+        private ObservableCollection<Estudiante> post;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnConsultar_Clicked(object sender, EventArgs e)
+        {
+            var contenido = await cliente.GetStringAsync(url);
+            List<Estudiante> datosPost = JsonConvert.DeserializeObject<List<Estudiante>>(contenido);
+
+            post = new ObservableCollection<Estudiante>(datosPost);
+            listarEstudiante.ItemsSource = post;
+
+
+        }
+    }
+}
